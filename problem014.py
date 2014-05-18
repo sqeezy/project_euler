ff2ed358d7745f6e2b32bd025a02b3709badf843
sqeezy <sqeezy@gmx.de>
2014-05-18 16:07:19 +0200

    Add solution for problem014.

--- problem014.py
+++ problem014.py
@@ -1,1 +1,23 @@
-__author__ = 'Anton'
+def next(n):
+    if n%2==0:
+        return n/2
+    else:
+        return 3*n+1
+
+def sequence(n):
+    seq = [n]
+    while n!=1:
+        n = next(n)
+        seq.append(n)
+    return seq
+
+maxLen = 0
+maxNum = 0
+for i in range(1,10**6):
+    curLen = len(sequence(i))
+    if curLen>maxLen:
+        maxLen=curLen
+        maxNum = i
+        print maxLen
+print maxLen
+print maxNum
