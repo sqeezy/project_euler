7b3d28f1d0613fa16f45bf116e52466c8a51cdd0
sqeezy <sqeezy@gmx.de>
2014-11-10 16:03:25 +0100

    Started solution for problem 18.

---
+++ problem018.py
@@ -0,0 +1,25 @@
+f = open("problem018-data.txt",'r')
+
+levels = []
+for rawLine in f:
+    numbers = rawLine.split(' ')
+    level = []
+    for number in numbers:
+        level.append(int(number))
+    levels.append(level)
+
+bestPathTillDepth = []
+way = []
+for i in range(0,len(levels)):
+    bestPathTillDepth.append(0)
+
+
+
+for level in range(0,len(levels)):
+    for number in range(0,len(levels[level])):
+        tryForBest = bestPathTillDepth[level-1]+levels[level][number]
+        if(tryForBest>bestPathTillDepth[level]):
+            bestPathTillDepth[level] = tryForBest
+
+print bestPathTillDepth
+print len(bestPathTillDepth)
